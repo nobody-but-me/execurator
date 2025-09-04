92b5b70785f55a128e1017f399e53e3369f6a35a
Daniel Nunes Teixeira <nunesteixeiradaniel12@gmail.com>
2025-09-04 15:21:52 -0300

    Reading and translating README files to HTML. Implementation of strrep and handle_sigint functions.

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <netinet/in.h>
 #include <stdbool.h>
 #include <unistd.h>
+#include <signal.h>
 #include <string.h>
 #include <assert.h>
 #include <stdlib.h>
@@ -43,7 +44,8 @@ const char *file_to_char(const char *file_path)
 #if VERBOSE == true
     printf("[INFO] : File have been loaded sucessfully. \n");
 #endif
-    return buffer;
+    const char *result = buffer;
+    return result;
     
 BLANK:
     fprintf(stderr, "[INFO] : File is blank. \n");
@@ -52,6 +54,37 @@ BLANK:
     buffer = (char *)malloc(sizeof(char));
     free(buffer);
     return NULL;
+}
+
+// TODO: change this the place of the function below.
+static char *strrep(char *orig, char *rep, char *with)
+{
+    int len_rep, len_with, len_front, count;
+    char *result, *ins, *tmp;
+    
+    if (!orig || !rep) return NULL;
+    
+    len_rep = strlen(rep);
+    if (len_rep == 0) return NULL;
+    if (!with) with = "";
+    len_with = strlen(with);
+    
+    ins = orig;
+    for (count = 0; (tmp = strstr(ins, rep)); ++count) {
+        ins = tmp + len_rep;
+    }
+    tmp = result = malloc(strlen(orig) + (len_with - len_rep) * count + 1);
+    if (!result) return NULL;
+    
+    while (count--) {
+        ins = strstr(orig, rep);
+        len_front = ins - orig;
+        tmp = strncpy(tmp, orig, len_front) + len_front;
+        tmp = strcpy(tmp, with) + len_with;
+        orig += len_front + len_rep;
+    }
+    strcpy(tmp, orig);
+    return result;
 }
 
 const char *translate_content()
@@ -66,20 +99,29 @@ const char *translate_content()
     RadownValue *main = lexer(&_tmp);
     char *content = "";
     while (main != NULL) {
-	if (main->type == 0) {
-	    int new_length = sizeof(char) * (strlen(content) + strlen(main->header_value));
-	    content = (char *)malloc(new_length);
-	    sprintf(content, "<h1>%s</h1>", main->header_value);
+	// TODO: Refactoring needed.
+	char *old_content = content;
+	char *new_content;
+	
+	if (main->type == TYPE_HEADER) {
+	    char *tag = "<h1></h1>";
+	    size_t length = sizeof(char) * (strlen(main->header_value) + strlen(tag) + 1);
+	    new_content = (char*)malloc(length);
+	    
+	    sprintf(new_content, "<h1>%s</h1>", main->header_value);
+	} else {
+	    char *tag = "<p></p>";
+	    size_t length = sizeof(char) * (strlen(main->header_value) + strlen(tag) + 1);
+	    new_content = (char*)malloc(length);
+	    
+	    sprintf(new_content, "<p>%s</p>", main->header_value);
 	}
-	else if (main->type == 1) {
-	    int new_length = sizeof(char) * (strlen(content) + strlen(main->para_value));
-	    content = (char *)malloc(new_length);
-	    sprintf(content, "<p>%s</p>", main->para_value);
-	}
+	
+	content = (char*)malloc((strlen(content) * 2) + strlen(new_content) + 1);
+	sprintf(content, "%s%s", old_content, new_content);
+	main = lexer(&_tmp);
     }
-    printf("[INFO] :  Content: %s.\n", content);
-    const char *result = content;
-    return result;
+    return content;
 }
 
 int handle_client(int client)
@@ -87,9 +129,10 @@ int handle_client(int client)
     char buffer[1024];
     read(client, buffer, sizeof(buffer));
     
-    translate_content();
+    // TODO: bad practice: casting away const-ness.
+    char *templ = (char *)file_to_char("../templates/template.html");
+    char *result = (char *)translate_content();
     
-    const char *templ = file_to_char("../templates/template.html");
     if (templ == NULL) {
 	fprintf(stderr, "[FAILED] : Template could not be loaded.\n");
 	close(client);
@@ -98,16 +141,39 @@ int handle_client(int client)
     const char *http_header = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n";
     char *html;
     
-    int new_length = sizeof(char) * (strlen(http_header) + strlen(templ));
+    int new_length = sizeof(char) * (strlen(http_header) + strlen(templ) + 1);
     html = (char *)malloc(new_length);
+    
+    templ = strrep(templ, "%t", result);
+    if (templ == NULL) {
+	fprintf(stderr, "[FAILED] : Program could not write content into template.\n");
+	return -1;
+    }
     sprintf(html, "%s%s", http_header, templ);
+    
+    printf("[INFO] html content: \n%s\n\n", html);
+    
     write(client, html, strlen(html));
     close(client);
+    free(html);
     return 0;
 }
 
-int main(int argv, char *argc[]) {
+volatile sig_atomic_t stop_server = 0;
+
+void handle_sigint(int sig)
+{
+    stop_server = 1;
+    return;
+}
+
+int main(int argv, char *argc[])
+{
     int server = socket(AF_INET, SOCK_STREAM, 0);
+    if (server == -1) {
+	printf("[FAILED] : TCP socket could not be created. \n");
+	return -1;
+    }
     struct sockaddr_in addr = {0};
     
     addr.sin_addr.s_addr = INADDR_ANY;
@@ -118,11 +184,19 @@ int main(int argv, char *argc[]) {
     listen(server, 10);
     
     printf("[INFO] : Server is running at http://localhost:4242 address.\n");
-    while (true) {
+    signal(SIGINT, handle_sigint);
+    while (!stop_server) {
 	int client = accept(server, NULL, NULL);
+	if (client == -1) {
+	    if (errno == EINTR) continue;
+	    else { perror("accept"); break; }
+	}
+	
 	if (handle_client(client) == -1) {
 	    break;
 	}
     }
+    close(server);
+    printf("[INFO] : Shutting down. Bye.\n");
     return 0;
 }
